9bb1435d601b59bdcbf1e245a2e2b6ec6f82a0c6
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-07-20 18:39:50 +0900

    memos#searchで返すjson整形

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -45,6 +45,25 @@ class MemosController < ApplicationController
     render json: @memos
   end
 
+  # GET /memos/search
+  def search
+    @get_memos = Memo.joins(:tag).where('tags.name = ?', params[:q])
+
+    @memos = []
+    @get_memos.each_with_index do | get_memo, index |
+      @memos[index] = {
+        id: get_memo.id,
+        color_code: get_memo.color_code,
+        comment: get_memo.comment,
+        URL: get_memo.url,
+        tag_name: get_memo.tag.name,
+        created_at: get_memo.created_at
+      }
+    end
+
+    render json: @memos
+  end
+
   # POST /memos
   def create
     # TODO: ログイン中のユーザーの user_id を取得する
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :color_files
   resources :users
 
+  # ファイルを指定してその中にあるメモを返す
   get 'files/:id', to: 'memos#show_inFile'
+
+  # タグ検索
+  get 'search', to: 'memos#search'
 end
