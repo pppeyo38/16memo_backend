9bfb2c970cf0415c8362afbbda2125514fd0a8e6
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-07-24 00:54:29 +0900

    firebase auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,10 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
+    # @users = User.all
+    @user = current_user
 
-    render json: @users
+    render json: @user
   end
 
   # GET /users/1
